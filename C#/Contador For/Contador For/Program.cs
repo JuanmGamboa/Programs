fb15eabf3b44ac82d35ed76a0b8af5b0500fdc76
Juan Manuel Gamboa Gonzalez <juan.gamboa@cetys.edu.mx>
2018-04-07 09:49:00 -0700

    Contar
    Contar  Números. De menor a mayor, mayor a menor.

---
+++ C#/Contador For/Contador For/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Contador_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            string entrada;
+            Console.WriteLine("Contador");
+            Console.WriteLine("¿Hasta que número desea contar?");
+            entrada = Console.ReadLine();
+            n = Convert.ToInt32(entrada);
+            for (i= 1; i<=n; i++)
+            {
+                Console.WriteLine("{0}", i); // Cuenta de 1 hasta n
+                Thread.Sleep(100);
+                
+            }
+            for (i = n; i >= 1; i--) // Cuenta regresiva de n hasta 1
+            {
+                Console.WriteLine("{0}", i);
+                Thread.Sleep(100);
+            }
+
+        }
+    }
+}
