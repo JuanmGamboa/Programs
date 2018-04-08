ed2898022214f07c648799db13bc3610f4323f55
Juan Manuel Gamboa Gonzalez <juan.gamboa@cetys.edu.mx>
2018-04-08 12:00:31 -0700

    Aerolinea
    Dar los valores e Imprimir la infromación

---
+++ C#/Aerolinea/Aerolinea/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerolinea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  manuel, juan;
+            string entrada, entrada2, entrada3;
+            Console.WriteLine("Escriba el nombre de la aerlonia.");
+            entrada = Console.ReadLine();
+            Convert.ToString(entrada);
+            Console.WriteLine("Escriba la Clave del vuelo");
+            entrada2 = Console.ReadLine();
+            Convert.ToString(entrada2);
+            Console.WriteLine("Ciudad a la que viaja");
+            entrada3 = Console.ReadLine();
+            Convert.ToString(entrada3);
+            Console.WriteLine("Nombre de la arelonia: {0}", entrada);
+            Console.WriteLine("Clave de vuelo: {0}", entrada2);
+            Console.WriteLine("Ciudad a la que viaja: {0}", entrada3);
+        }
+    }
+}
